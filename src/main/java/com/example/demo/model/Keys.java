27e441f9d6947d75d8c9a8b37f1674e1eb3b1e45
xhaktmchl <xhaktmchl@naver.com>
2022-10-06 16:08:47 +0900

    first commit

---
+++ src/main/java/com/example/demo/model/Keys.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import java.util.List;
+
+public class Keys {
+
+    private List<Key> keys;
+
+    public List<Key> getKeys() {
+        return keys;
+    }
+
+    public void setKeys(List<Key> keys) {
+        this.keys = keys;
+    }
+
+}
